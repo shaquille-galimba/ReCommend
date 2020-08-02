8b3c41bb248183f549b1d7275d9747e96fd83265
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-02 01:42:57 -0400

    add password encryption to user model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
 	has_many :brands
+	has_many :recommends, through: :brands
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_secure_password
 	has_many :recommends
 	has_many :brands, through: :recommends
 	has_many :categories, through: :brands
