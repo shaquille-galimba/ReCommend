e3f93208251d05c53b9f1b0efc1a81bf34037015
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-04 01:59:36 -0400

    add current_user and logged_in? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_user
+
+	def current_user
+		@user = User.find_by(id: session[:user_id])
+	end
+
+	def logged_in?
+		current_user != nil
+	end
 
 	def welcome
 
