52d6691b1187a98e99576158e0af871e1a86f38e
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-04 02:35:50 -0400

    add user#create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,23 @@ class UsersController < ApplicationController
 	end
 
 	def create
-
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
+		else
+			render new
+		end
 	end
 
 	def show
 		require_logged_in
 		@user = User.find_by(id: params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
 	end
 end
