abcae54208398ebec0cef825887c70823cc8e985
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-04 01:41:52 -0400

    add simple login using username and password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
 class SessionsController < ApplicationController
 
 	def new
-		
+
 	end
 
 	def create
-
+		@user = User.find_by(username: params[:user][:username])
+		if @user && @user.authenticate(params[:user][:password])
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
+		else
+			redirect_to login_path
+		end
 	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,2 @@
 module UsersHelper
-
-	def show
-		@user = User.find_by(params[:id])
-	end
 end
