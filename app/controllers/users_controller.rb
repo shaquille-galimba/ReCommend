cf6fc333c5d394559baadcb407c406ef8fb65cf3
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-05 01:04:56 -0400

    update user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
+		params.require(:user).permit(:email, :username, :password, :password_confirmation)
 	end
 end
