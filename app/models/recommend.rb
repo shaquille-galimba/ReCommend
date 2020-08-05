a6000d1cae88e828165d96a6711842075674c7c8
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-05 02:54:07 -0400

    add helper method

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -2,5 +2,13 @@ class Recommend < ApplicationRecord
   belongs_to :user
   belongs_to :brand
 
-	# scope :categorize, -> { includes(brand: :category).order('categories.name DESC')}
+	scope :categorize, -> { includes(brand: :category).order('categories.name DESC')}
+
+	def user_name
+		user.username
+	end
+
+	def brand_name
+		brand.name
+	end
 end
