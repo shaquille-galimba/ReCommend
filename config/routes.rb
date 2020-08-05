849866c1ca89f05c2e7bbf884b26de8bbf471ca7
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-05 03:24:56 -0400

    change root path, add welcome message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root 'application#welcome'
+	root 'recommends#index'
 	get '/signup' => 'users#new'
 	post '/signup' => 'users#create'
 	get '/login' => 'sessions#new'
