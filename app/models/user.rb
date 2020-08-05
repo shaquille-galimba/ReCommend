af9597704bfbd3d419f9b3ba678ab9826f0240e4
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-05 01:14:30 -0400

    add format validation of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :email, uniqueness: true
 	validates :username, uniqueness: true
+	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 	has_many :recommends
 	has_many :brands, through: :recommends
 	has_many :categories, -> { distinct }, through: :brands
