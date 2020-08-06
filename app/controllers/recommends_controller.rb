e5e665cf1caa764fe4235bfc234ff1e34c0cdaf3
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-06 02:54:37 -0400

    edit new controller action and recommend_params

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -2,11 +2,28 @@ class RecommendsController < ApplicationController
 
 	def new
 		@recommend = Recommend.new
-		brand = @recommend.brand = Brand.new
-		brand.category = Category.new
+		@recommend.build_brand.build_category
+	end
+
+	def create
+		
 	end
 
 	def index
 		@recommends = Recommend.all
+	end
+
+	private
+
+	def recommend_params
+		params.require(:recommend).permit(
+			:comment,
+			brand_attributes: [
+				:name,
+				category_attributes: [
+					:name
+				]
+			]
+		)
 	end
 end
