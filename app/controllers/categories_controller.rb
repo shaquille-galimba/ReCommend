88fdd0d53a8e5b3e701ac6d73052d6d548e4be7c
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-07 04:00:29 -0400

    add flash messages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,12 @@ class CategoriesController < ApplicationController
 	end
 
 	def show
-
+		@category = Category.find_by_id(params[:id])
+		if @category
+			@brands = @category.brands.most_popular
+		else
+			flash[:alert] = "Category not found"
+			redirect_to root_path
+		end
 	end
 end
