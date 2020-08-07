d5d98ff576d6731b2446074a03994ec6d442f680
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-07 02:31:40 -0400

    only allow current user to create recommends

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,12 +1,19 @@
 class RecommendsController < ApplicationController
 
 	def new
+		require_logged_in
 		@recommend = Recommend.new
 		@recommend.build_brand.build_category
 	end
 
 	def create
-
+		@recommend = Recommend.new(recommend_params)
+		@recommend.user = current_user
+		if @recommend.save
+			redirect_to recommend_path(@recommend)
+		else
+			render :new
+		end
 	end
 
 	def show
@@ -21,6 +28,7 @@ class RecommendsController < ApplicationController
 
 	def recommend_params
 		params.require(:recommend).permit(
+			:user,
 			:comment,
 			brand_attributes: [
 				:name,
