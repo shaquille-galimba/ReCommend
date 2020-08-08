316bda95966ab46723c1a207c78847ff4f0bc83b
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-08 01:15:08 -0400

    update index

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -25,8 +25,7 @@ class RecommendsController < ApplicationController
 
 	def index
 		if params[:user_id]
-			user = User.find_by_id(params[:user_id])
-			if user
+			if user = User.find_by_id(params[:user_id])
 				@recommends = user.recommends
 			else
 				flash[:alert] = "User not found"
