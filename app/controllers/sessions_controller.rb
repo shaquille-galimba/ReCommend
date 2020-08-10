3efd4902b906333a9490f2ec54b97eca3c607ee9
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-10 05:28:21 -0400

    change redirect after submitting form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 		@user = User.find_by(username: params[:user][:username])
 		if @user && @user.authenticate(params[:user][:password])
 			session[:user_id] = @user.id
-			redirect_to user_path(@user)
+			redirect_to recommends_path
 		else
 			redirect_to login_path
 		end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,18 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to user_path(@user)
+			redirect_to recommends_path
 		else
 			render :new
 		end
+	end
+
+	def edit
+		@user = User.find_by(id: params[:id])
+	end
+
+	def update
+		@user = User.find_by(id: params[:id])
 	end
 
 	def show
