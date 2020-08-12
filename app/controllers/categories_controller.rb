58a8d57f5c323c4fb6d083ded9a0ca220ed2bed5
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-12 06:11:23 -0400

    add most popular scope

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
 	def index
-		@categories = Category.all
+		@categories = Category.most_popular
 	end
 
 	def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
 	has_many :brands
 	has_many :recommends, through: :brands
 	# has_many :users, through: :brands
+
+	scope :most_popular, -> {joins(:recommends).group('categories.id').order('count(categories.id) desc')}
 end
