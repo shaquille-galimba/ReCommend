83bb73132f1efc0c440541e70cd744acb2ea49c3
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-14 05:13:07 -0400

    add error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :email, uniqueness: true
 	validates :username, uniqueness: true
-	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true, :message => "can't have symbols"
 	has_many :recommends, dependent: :delete_all
 	has_many :brands, through: :recommends
 	has_many :categories, -> { distinct }, through: :brands
