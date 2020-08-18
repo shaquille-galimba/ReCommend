353b367f9f93ae31c5b8747fd83e681d8d56ead0
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-18 09:19:42 -0400

    refactor to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+	def show_edit_info_and_delete_account_button
+		if @user == current_user
+			content_tag(:li) do
+				link_to("Edit information", edit_user_path(@user))
+			end +
+			content_tag(:li) do
+				link_to "Delete account", user_path(@user), method: :delete, data: {confirm: "Are you sure you want to delete your account?"}
+			end
+		end
+	end
 end
