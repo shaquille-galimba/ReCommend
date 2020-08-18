11b505cddab52e36f6fd3e032d2d2bc060345ba3
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-18 06:51:20 -0400

    refactor display header in new recommend views

--- app/helpers/recommends_helper.rb
+++ app/helpers/recommends_helper.rb
@@ -1,2 +1,12 @@
 module RecommendsHelper
+	def new_display_header
+		if @brand
+	 		content_tag(:h1, "Recommend #{@brand.name}") +
+	 		content_tag(:strong, "Category: #{@brand.category_name}")
+ 	  elsif @category
+ 		  content_tag(:h1, "#{@category.name} recommendation")
+ 	  else
+ 		 content_tag(:h1, "New recommendation!")
+ 	  end
+	end
 end
