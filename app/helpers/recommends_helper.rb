079af962d030ac50b0dca32439e29274a849b74a
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-18 07:48:30 -0400

    refactor index display header into helper

--- app/helpers/recommends_helper.rb
+++ app/helpers/recommends_helper.rb
@@ -9,4 +9,30 @@ module RecommendsHelper
  		 content_tag(:h1, "New recommendation!")
  	  end
 	end
+
+	def index_display_header
+		if @user
+			content_tag(:h1, "#{@user.username}") +
+			content_tag(:div, class: "navbar mini") do
+				content_tag(:ul) do
+					content_tag(:li, class: "separate") do
+						link_to 'Info', user_path(@user)
+					end +
+					content_tag(:div, class: "info right") do
+						render partial: "shared/search", locals: {path:user_recommends_path(@user)}
+					end
+				end
+			end
+		else
+			content_tag(:h1, "Welcome to Recommend!") +
+			content_tag(:div, class: "navbar mini") do
+				content_tag(:ul) do
+					content_tag(:div, class: "info right") do
+						render partial: "shared/search", locals: {path: recommends_path}
+					end
+				end
+			end
+		end
+	end
+
 end
