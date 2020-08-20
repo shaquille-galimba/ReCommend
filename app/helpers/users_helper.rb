3c3b6e80020c78a6db0b855d05ee7ef55a3d7abd
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-08-20 06:15:07 -0400

    only allow user to edit their username so that oauth user won't edit their email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 	def show_edit_info_and_delete_account_button
 		if @user == current_user
 			content_tag(:li) do
-				link_to("Edit information", edit_user_path(@user))
+				link_to("Edit username", edit_user_path(@user))
 			end +
 			content_tag(:li) do
 				link_to "Delete account", user_path(@user), method: :delete, data: {confirm: "Are you sure you want to delete your account?"}
